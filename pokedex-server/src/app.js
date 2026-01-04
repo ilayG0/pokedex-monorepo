81d26e7ec21e214cdb327ad954a3fba1c63ea934
Ilay Gozlan <ilay.gozlan@moveo.co.il>
2026-01-04 14:21:21 +0200

    v11

--- pokedex-server/src/app.js
+++ pokedex-server/src/app.js
@@ -12,7 +12,7 @@ function createApp() {
   app.use(express.json());
 
   app.get("/health", (req, res) => {
-    res.json({ status: "ok" ,version: "10"});
+    res.json({ status: "ok" ,version: "11"});
   });
 
   app.use("/api/auth", authRoutes);
