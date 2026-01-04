99331a1679716fd677e36dcb6353bfb502ee9f08
Ilay Gozlan <ilay.gozlan@moveo.co.il>
2026-01-04 13:45:56 +0200

    server

--- pokedex-server/ecosystem.config.js
+++ pokedex-server/ecosystem.config.js
@@ -2,13 +2,14 @@ module.exports = {
   apps: [
     {
       name: "pokedex-server",
-      script: "npm",
-      args: "run dev",
+      script: "src/server.js",
+      cwd: "/home/ubuntu/pokedex-server",
       instances: 1,
       exec_mode: "fork",
       env: {
         NODE_ENV: "production",
-      },
-    },
-  ],
+        PORT: 3000
+      }
+    }
+  ]
 };
